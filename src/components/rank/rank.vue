f2b6594e197e21f30f6f531f5e1e5c1735c63c99
Liuxi <626506518@qq.com>
2018-10-21 23:07:55 +0800

    2018-10-21

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        推荐页面
+    </div>
+</template>
+
+<script type="ecmascript-6">
+</script>
+
+<styles scoped lang="stylus" rel="stylesheet-stylus">
+</styles>
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        歌手页面
+    </div>
+</template>
+
+<script type="ecmascript-6">
+</script>
+
+<styles scoped lang="stylus" rel="stylesheet-stylus">
+</styles>
