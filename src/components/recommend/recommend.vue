e2ac01728b852138f31cb9d5329e2ac03817f823
Liuxi <626506518@qq.com>
2018-11-01 00:50:10 +0800

    4.7,遺留bug:unexpected tooken : 未解決

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -21,7 +21,7 @@
 
 <script type="text/ecmascript-6">
   import Slider from 'base/slider/slider'
-  import {getRecommend} from 'api/recommend'
+  import {getRecommend, getDistList} from 'api/recommend'
   import {ERR_OK} from 'api/config'
 
   export default {
@@ -36,12 +36,20 @@
     },
     created() {
       this._getRecommend()
+      this._getDistList()
     },
     methods: {
       _getRecommend() {
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
             this.recommends = res.data.slider
+          }
+        })
+      },
+      _getDistList() {
+        getDistList().then((res) => {
+          if (res.code === ERR_OK) {
+            console.log(res.data.list)
           }
         })
       }
